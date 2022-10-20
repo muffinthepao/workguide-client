c1f6d951a010e55e2689a92ebce36efba3a0f2ad
Mervin Poh <mervpoh@gmail.com>
2022-10-20 22:13:20 +0800

    feat: submit video answer

--- src/pages/webcamtest.jsx
+++ src/pages/webcamtest.jsx
@@ -9,7 +9,9 @@ const WebcamStreamCapture = () => {
   const [capturing, setCapturing] = useState(false);
   const [recordedChunks, setRecordedChunks] = useState([]);
   const [previewUrl, setPreviewUrl] = useState("");
+  const [createdBlob, setCreatedBlob] = useState(null);
 
+  const userId = 2;
   const questionId = 1;
   const baseAnswerURL = `${process.env.REACT_APP_QNS_BASE_URL}/${questionId}/answers`;
 
@@ -33,10 +35,12 @@ const WebcamStreamCapture = () => {
       console.log("data available");
 
       const blob = new Blob(newRecordedChunks, {
-        type: "video/webm",
+        type: "video/mp4",
+        codecs:"avc1.424028, mp4a.40.2"
       });
       const url = URL.createObjectURL(blob);
       console.log("url: ", url);
+      setCreatedBlob(blob);
       setPreviewUrl(url);
     }
   };
@@ -50,6 +54,7 @@ const WebcamStreamCapture = () => {
     window.URL.revokeObjectURL(previewUrl);
     setRecordedChunks([]);
     setPreviewUrl("");
+    setCreatedBlob(null);
   };
 
   const handleSubmit = async () => {
@@ -60,10 +65,24 @@ const WebcamStreamCapture = () => {
     // so userId, etc will remain in req.body
     // video data will go to req.files
 
-    console.log("clicked submit")
+    console.log("clicked submit");
 
     try {
-      const response = await axios.post(baseAnswerURL);
+      // // There are two ways to upload forms with multipart/form-data encoding. 
+      // // The first is by using the enctype attribute: enctype='multipart/form-data'
+      // // But since i dont have a form nor input fields in my html, I need to use the second way. Second way being to create my own FormData() 
+      const form = new FormData();
+      form.append("questionId", questionId);
+      form.append("userId", userId);
+      form.append("file", createdBlob);
+      const response = await axios.post(baseAnswerURL,  form );
+
+      // const response = await axios.post(baseAnswerURL, {
+      //   questionId,
+      //   userId,
+      //   file: createdBlob
+      // } );
+  
 
       if (response.status === 200 || response.status === 201) {
         toast.success("Answer Submitted");
@@ -77,13 +96,6 @@ const WebcamStreamCapture = () => {
   return (
     <>
       {/* if left is true, return right. Right side is a jsx therefore always true */}
-      {/* {previewUrl !== "" && (
-        <div>
-          <video controls className="videoPreview" src={previewUrl}></video>
-          <button onClick={handleRetake}>Retake</button>
-        </div>
-      )} */}
-
       {previewUrl !== "" ? (
         <div>
           <video controls className="videoPreview" src={previewUrl}></video>
