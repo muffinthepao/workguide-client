1fe1086c297002c5e97cfa4e1e97de1a15281580
Mervin Poh <mervpoh@gmail.com>
2022-10-26 17:22:14 +0800

    basic navbar

---
+++ src/pages/landingPage.jsx
@@ -0,0 +1,11 @@
+import React, { useRef, useState } from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <p>this is the landing page</p>
+    </>
+  );
+};
+
+export default LandingPage;
