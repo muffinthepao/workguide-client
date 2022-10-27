25afc5890a1c4f7c69c982b8f89ff29f97b98ebd
Mervin Poh <mervpoh@gmail.com>
2022-10-27 21:11:25 +0800

    chore: added NavLink to Navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { NavLink} from "react-router-dom";
+
 export default function Navbar() {
     return (
     <>
     <nav id="header" className="fixed w-full px-3 z-30 top-0 text-white bg-yellow-400">
       <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div className="pl-4 flex items-center">
-          <a className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+          <NavLink to="/"className="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl">
             <svg className="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512.005 512.005">
               <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502" transform="matrix(1,0,0,1,0,0)" />
               <path
@@ -13,7 +15,7 @@ export default function Navbar() {
               />
             </svg>
             Workguide
-          </a>
+          </NavLink>
         </div>
         <div className="block lg:hidden pr-4">
           <button id="nav-toggle" className="flex items-center p-1 text-pink-800 hover:text-gray-900 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
@@ -26,21 +28,22 @@ export default function Navbar() {
         <div className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
           <ul className="list-reset lg:flex justify-end flex-1 items-center">
             <li className="mr-3">
-              <a className="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Active</a>
+              <NavLink to="/questions" className="inline-block font-bold text-black no-underline hover:underline py-2 px-4 hover:scale-105 duration-300 ease-in-out">QUESTIONS!</NavLink>
             </li>
             <li className="mr-3">
-              <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+              <NavLink to="/guide"className="inline-block text-black no-underline hover:underline py-2 px-4 hover:scale-105 duration-300 ease-in-out" >BECOME A GUIDE</NavLink>
             </li>
             <li className="mr-3">
-              <a className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">link</a>
+              <NavLink to="/login"className="inline-block text-black no-underline hover:underline py-2 px-4 hover:scale-105 duration-300 ease-in-out" >LOGIN</NavLink>
             </li>
           </ul>
-          <button
+          <NavLink
+            to="/join"
             id="navAction"
-            className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
           >
-            Action
-          </button>
+            JOIN
+          </NavLink>
         </div>
       </div>
       <hr className="border-b border-gray-100 opacity-25 my-0 py-0" />
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,11 @@
-import React, { useRef, useState } from "react";
+// import React, { useRef, useState } from "react";
+import React from "react";
 
 import HeroImage from "../LandingPage/workplace-stress.jpeg"
 import ScrollDownGif from "../LandingPage/down-arrow.gif"
-import ClarityPic from "../LandingPage/clarity4.png"
+import ClarityPic from "../LandingPage/clarity.png"
 import CompetencyPic from "../LandingPage/competence.png"
-import ConnectionsPic from "../LandingPage/credible-connections1.png"
+import ConnectionsPic from "../LandingPage/connections.png"
 
 const LandingPage = () => {
   return (
@@ -20,14 +21,14 @@ const LandingPage = () => {
           </h1>
           <p className="leading-normal text-2xl mb-8">
             Don't do it alone. Get a Local Guide
-          </p>
+          </p> 
         </div>
         <div className="w-full md:w-7/12 pt-4 text-center">
           {/* <img className="w-full md:w-4/5 z-50 text-center flex justify-center" src="https://domf5oio6qrcr.cloudfront.net/medialibrary/9764/GettyImages-936117884.jpg" /> */}
-          <img src={HeroImage} style={{objectFit: "cover"}}/>
+          <img src={HeroImage} style={{objectFit: "cover"}} alt="hero.img"/>
         </div>
         <div className="w-full text-center">
-          <img className="mx-auto" src={ScrollDownGif} style={{width: "200px", height: "70px", objectFit: "cover"}}/>
+          <img className="mx-auto" src={ScrollDownGif} style={{width: "200px", height: "70px", objectFit: "cover"}} alt="scrolldown.gif"/>
         </div>
       </div>
     </div>
@@ -68,13 +69,13 @@ const LandingPage = () => {
             </p>
           </div>
           <div className="w-full sm:w-1/2 p-6">
-            <img src={ClarityPic} />
+            <img src={ClarityPic} alt="clarity.img"/>
           </div>
           
         </div>
         <div className="flex flex-wrap flex-col-reverse sm:flex-row">
           <div className="w-full sm:w-1/2 p-6 mt-6">
-            <img src={CompetencyPic}/>
+            <img src={CompetencyPic} alt="competency.img"/>
           </div>
           <div className="w-full sm:w-1/2 p-6 mt-6 flex">
             <div className="my-auto">
@@ -98,7 +99,7 @@ const LandingPage = () => {
             </p>
           </div>
           <div className="w-full sm:w-1/2 p-6">
-            <img src={ConnectionsPic} />
+            <img src={ConnectionsPic} alt="connections.img"/>
           </div>
           
         </div>
@@ -150,110 +151,8 @@ const LandingPage = () => {
         </div>
       </div>
     </section>
-    {/* <section className="bg-gray-100 py-8">
-      <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
-        <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
-          Pricing
-        </h2>
-        <div className="w-full mb-4">
-          <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-        </div>
-        <div className="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
-          <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-            <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-              <div className="p-8 text-3xl font-bold text-center border-b-4">
-                Free
-              </div>
-              <ul className="w-full text-center text-sm">
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-              </ul>
-            </div>
-            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
-                £0
-                <span className="text-base">for one user</span>
-              </div>
-              <div className="flex items-center justify-center">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
-            <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-              <div className="w-full p-8 text-3xl font-bold text-center">Basic</div>
-              <div className="h-1 w-full gradient my-0 py-0 rounded-t"></div>
-              <ul className="w-full text-center text-base font-bold">
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-              </ul>
-            </div>
-            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div className="w-full pt-6 text-4xl font-bold text-center">
-                £x.99
-                <span className="text-base">/ per user</span>
-              </div>
-              <div className="flex items-center justify-center">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
-            <div className="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
-              <div className="p-8 text-3xl font-bold text-center border-b-4">
-                Pro
-              </div>
-              <ul className="w-full text-center text-sm">
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-                <li className="border-b py-4">Thing</li>
-              </ul>
-            </div>
-            <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-              <div className="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
-                £x.99
-                <span className="text-base">/ per user</span>
-              </div>
-              <div className="flex items-center justify-center">
-                <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section> */}
-
-    <svg className="wave-top" viewBox="0 0 1439 147" version="1.1">
-      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g transform="translate(-1.000000, -14.000000)" fill-rule="nonzero">
-          <g className="wave" fill="#f8fafc">
-            <path
-              d="M1440,84 C1383.555,64.3 1342.555,51.3 1317,45 C1259.5,30.824 1206.707,25.526 1169,22 C1129.711,18.326 1044.426,18.475 980,22 C954.25,23.409 922.25,26.742 884,32 C845.122,37.787 818.455,42.121 804,45 C776.833,50.41 728.136,61.77 713,65 C660.023,76.309 621.544,87.729 584,94 C517.525,105.104 484.525,106.438 429,108 C379.49,106.484 342.823,104.484 319,102 C278.571,97.783 231.737,88.736 205,84 C154.629,75.076 86.296,57.743 0,32 L0,0 L1440,0 L1440,84 Z"
-            ></path>
-          </g>
-          <g transform="translate(1.000000, 15.000000)" fill="#FFFFFF">
-            <g transform="translate(719.500000, 68.500000) rotate(-180.000000) translate(-719.500000, -68.500000) ">
-              <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
-              <path
-                d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-                opacity="0.100000001"
-              ></path>
-              <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" opacity="0.200000003"></path>
-            </g>
-          </g>
-        </g>
-      </g>
-    </svg>
-    <section className="container bg-yellow-400 mx-auto text-center py-6 mb-12">
+  
+    <section className="container bg-yellow-400 mx-auto text-center py-6 my-12">
       <h2 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
         JOIN TODAY!
       </h2>
