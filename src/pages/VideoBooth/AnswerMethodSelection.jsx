f2e3da9b90efa5bf564c391ef4fea0805a178412
Mervin Poh <mervpoh@gmail.com>
2022-10-30 21:46:02 +0800

    feat: submit answer component

--- src/pages/VideoBooth/AnswerMethodSelection.jsx
+++ src/pages/VideoBooth/AnswerMethodSelection.jsx
@@ -1,52 +1,91 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { icon, regular, solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import {
+  icon,
+  regular,
+  solid,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
 
 import ButtonStandard from "../../components/ButtonStandard";
 
 export default function AnswerMethodSelection() {
-
-
   return (
     <>
-      <div className="relative pt-24 bg-no-repeat bg-cover bg-center mx-10">
-        <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-          <div className="flex flex-wrap w-full flex-col items-center text-center py-4 border-bottom-width: 4px; border-b-8 border-b-solid">
-            <h1 className="sm:text-3xl text-3xl font-medium title-font mb-2 text-black">
+      <section class="text-gray-600 body-font mx-auto">
+        <div class="container px-5 py-24 pb-4 mx-auto">
+          <div class="flex flex-col text-center w-full mb-20">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-500">
               SUBMIT ANSWER
             </h1>
-            <p className="lg:w-1/2 w-full leading-relaxed text-black">
-              Select ONE method of Answering the Question
+            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Select ONE of the three options to submit and answer
             </p>
           </div>
-          <div className="md:w-64 :mb-0md mb-6 flex-shrink-0 flex md:flex-col md: space-y-5 sm:flex-row sm">
-          <span className="inline-block p-3 rounded bg-slate-50 text-indigo-500 text-xs font-medium tracking-widest hover:border-2 hover:border-indigo-500 duration-300 ease-in-out">
-            <FontAwesomeIcon
-              className="pr-3"
-              icon={icon({ name: "chevron-up", style: "solid" })}
-            />
-            VIDEO BOOTH
-          </span>
-
-          <span className="inline-block p-3 rounded bg-slate-50 text-indigo-500 text-xs font-medium tracking-widest hover:border-2 hover:border-indigo-500 duration-300 ease-in-out">
-            <FontAwesomeIcon
-              className="pr-3"
-              icon={icon({ name: "chevron-up", style: "solid" })}
-            />
-            VIDEO UPLOAD
-          </span>
-
-          <span className="inline-block p-3 rounded bg-slate-50 text-indigo-500 text-xs font-medium tracking-widest hover:border-2 hover:border-indigo-500 duration-300 ease-in-out">
-            <FontAwesomeIcon
-              className="pr-3"
-              icon={icon({ name: "chevron-up", style: "solid" })}
-            />
-            SUBMIT URL
-          </span>
+          <div class="flex flex-wrap -m-4 text-center">
+            <div class="p-4 md:w-1/3 sm:w-1/2 w-full">
+              <div class="border-2 border-slate-300 text-slate-400 px-4 py-6 rounded-lg hover:scale-105 duration-300 ease-in-out hover:border-yellow-400 hover:text-yellow-400">
+                <FontAwesomeIcon
+                  className="mb-3"
+                  icon={solid("video")}
+                  style={{ fontSize: "3rem" }}
+                />
+                <p class="title-font font-medium text-3xl">
+                  Video Booth
+                </p>
+                <p class="leading-relaxed">Guided video booth</p>
+              </div>
+            </div>
+            <div class="p-4 md:w-1/3 sm:w-1/2 w-full">
+            <div class="border-2 border-slate-300 text-slate-400 px-4 py-6 rounded-lg hover:scale-105 duration-300 ease-in-out hover:border-yellow-400 hover:text-yellow-400">
+                <FontAwesomeIcon
+                  className="mb-3"
+                  icon={solid("upload")}
+                  style={{ fontSize: "3rem" }}
+                />
+                <h2 class="title-font font-medium text-3xl">
+                  Upload File(s)
+                </h2>
+                <p class="leading-relaxed">Upload up to 4x video parts</p>
+              </div>
+            </div>
+            <div class="p-4 md:w-1/3 sm:w-1/2 w-full">
+            <div class="border-2 border-slate-300 text-slate-400 px-4 py-6 rounded-lg hover:scale-105 duration-300 ease-in-out hover:border-yellow-400 hover:text-yellow-400">
+                <FontAwesomeIcon
+                  className="mb-3"
+                  icon={solid("link")}
+                  style={{ fontSize: "3rem" }}
+                />
+                <h2 class="title-font font-medium text-3xl">
+                  URL
+                </h2>
+                <p class="leading-relaxed">Submit an answer via video URL</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section class="text-gray-600 body-font mx-auto">
+        <div class="container px-5 mx-auto">
+          <div class="flex flex-wrap justify-center -m-4 text-center">
+            <div class="p-4 md:w-1/3 sm:w-1/2 w-full">
+              <div class="border-2 border-gray-300 px-4 py-6 rounded-lg text-gray-400 hover:border-gray-500 hover:text-gray-600 ">
+                <FontAwesomeIcon
+                  className="mb-3"
+                  icon={solid("left-long")}
+                  style={{ fontSize: "3rem" }}
+                />
+                <h2 class="title-font font-medium text-3xl">
+                  BACK
+                </h2>
+                <p class="leading-relaxed">To question</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
     </>
   );
 }
