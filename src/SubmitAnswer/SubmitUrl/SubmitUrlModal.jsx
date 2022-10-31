80dd3b55ccd83d0ee161a06e7df56aa5342ee7ef
Mervin Poh <mervpoh@gmail.com>
2022-10-31 22:12:00 +0800

    feat: URL Answer Submit Joi Validation

--- src/SubmitAnswer/SubmitUrl/SubmitUrlModal.jsx
+++ src/SubmitAnswer/SubmitUrl/SubmitUrlModal.jsx
@@ -8,8 +8,6 @@ import { UrlSchema } from "../SubmitUrl/SubmitUrlValidation";
 
 export default function SubmitUrlModal({showUrlModal, setShowUrlModal }) {
 
-
-
   const {
     register,
     handleSubmit,
@@ -21,6 +19,10 @@ export default function SubmitUrlModal({showUrlModal, setShowUrlModal }) {
       url: "https://youtu.be/yvoO_ErTy6Q?t=11",
     },
   });
+
+  async function onSubmit(data) {
+    console.log("data: ", data)
+  }
 
   return (
     <>
@@ -45,31 +47,33 @@ export default function SubmitUrlModal({showUrlModal, setShowUrlModal }) {
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <textarea
-                    placeholder="Paste video URL here"
-                    id="url"
-                    name="url"
-                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowUrlModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowUrlModal(false)}
-                  >
-                    Submit
-                  </button>
-                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="relative p-6 flex-auto">
+                    <textarea
+                      placeholder="Paste video URL here"
+                      id="url"
+                      {...register("url")}
+                      className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                    <p className="text-red-500 text-left">{errors.url?.message}</p>
+                  </div>
+                  {/*footer*/}
+                  <div className="flex items-center justify-end p-6 rounded-b">
+                    <button
+                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowUrlModal(false)}
+                    >
+                      Close
+                    </button>
+                    <button
+                      className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
